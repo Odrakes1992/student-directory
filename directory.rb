69953c4e57ac040dfd4fd1b8ebc6ecb12acd3e06
Osneil Drakes <osneil-drakes@hotmail.com>
2021-08-18 23:33:40 +0100

    Refactoring

--- directory.rb
+++ directory.rb
@@ -14,77 +14,111 @@ students = [
 ]
 =end
 
+@students = []
+
 def input_students
 
-puts "Please enter the names of the students"
-puts "To finish, just hit return twice"
-#create an empty array
-students = []
-#get the first name
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  #create an empty array
+  
+  #get the first name
 
-name = gets.chomp
-#while thename is not empty, repeat this code
+  name = gets.chomp
+  #while thename is not empty, repeat this code
 
 
-while !name.empty? do
-#add the student hash to the array
-puts "Now type in the cohort"
-cohort = gets.chomp
+  while !name.empty? do
+  #add the student hash to the array
+    puts "Now type in the cohort"
+    cohort = gets.chomp
 
-if !cohort.empty? && students.length > 1
-  students << {name: name, cohort: cohort}
-  puts "Now we have #{students.count} students"
-elsif cohort.empty? && students.length > 1
-  students << {name: name, cohort: :november}
-  puts "Now we have #{students.count} students"
-elsif cohort.empty? && students.length <= 1
-  students << {name: name, cohort: :november}
-  puts "Now we have #{students.count} student"
-elsif !cohort.empty? && students.length <= 1
-  students << {name: name, cohort: cohort}
-  puts "Now we have #{students.count} student"
-end
+    if !cohort.empty? && @students.length > 1
+      @students << {name: name, cohort: cohort}
+      puts "Now we have #{@students.count} students"
+    elsif cohort.empty? && @students.length > 1
+      @students << {name: name, cohort: :november}
+      puts "Now we have #{@students.count} students"
+    elsif cohort.empty? && @students.length <= 1
+      @students << {name: name, cohort: :november}
+      puts "Now we have #{@students.count} student"
+    elsif !cohort.empty? && @students.length <= 1
+      @students << {name: name, cohort: cohort}
+      puts "Now we have #{@students.count} student"
+    end
 
-name = gets.chomp
+    name = gets.chomp
 
-end
-students
+  end
+  @students
 
 end
 
 
 def print_header
-puts "The students of Villains Academy"
-puts "---------------"
+  puts "The students of Villains Academy"
+  puts "---------------"
 end
 
-def print(students)
+def print
 
-puts "What cohort do you want to see, if it doesn't exist nothing will show"  
-cohort = gets.chomp
-
-while cohort.empty?
-  puts "Try again"
+  puts "What cohort do you want to see, if it doesn't exist nothing will show"  
   cohort = gets.chomp
-    if !cohort.empty?
-      break
-    end
-end
 
-students.map do |student|
-  if student[:cohort] == cohort
-    string = " #{student[:name]} (#{student[:cohort]}-cohort)"
-    puts string.center(18)
+  while cohort.empty?
+    puts "Try again"
+    cohort = gets.chomp
+      if !cohort.empty?
+        break
+      end
   end
-end  
+
+  @students.map do |student|
+    if student[:cohort] == cohort
+      string = " #{student[:name]} (#{student[:cohort]}-cohort)"
+      puts string.center(18)
+    end
+  end  
 end
 
-def print_footer(names)
-puts "Overall, we have #{names.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+def interactive_menu
+  loop do 
+    print_menu
+    process(gets.chomp)
+  end
+
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print
+  print_footer
+end
+
+def process(selection)
+  case selection 
+  when "1"
+    students = input_students
+  when "2"
+    print_header
+    print
+    print_footer
+  when "9"
+    exit # this will cause the program to terminate
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+
+interactive_menu
